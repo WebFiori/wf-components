4e2ce801a8ac2bf7ffaba1c78ba1dc39f073ab49
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-27 22:42:47 +0300

    Update Heading.php

--- wfc/ui/vuetify/Heading.php
+++ wfc/ui/vuetify/Heading.php
@@ -2,19 +2,36 @@
 namespace wfc\ui\vuetify;
 
 use webfiori\ui\HTMLNode;
-
+/**
+ * A class that can be used to add a top heading to the
+ * page or inside a section.
+ */
 class Heading extends HTMLNode {
     private $headingNode;
+    /**
+     * Creates new instance of the class.
+     * 
+     * @param HTMLNode|string $title A string that represents page title.
+     * This also can be an object of type 'HTMLNode'
+     * 
+     * @param int $level Heading level. A value between 1 and 6 inclusive.
+     */
     public function __construct($title, $level = 1) {
         parent::__construct('v-row', [
             'class' => 'my-10'
         ]);
-        $level = $level >= 1 && $level <= 6 ? $level : 1;
+        $xlevel = $level >= 1 && $level <= 6 ? $level : 1;
         $this->headingNode = $this->addChild('v-col', [
             'cols' => 12
-        ])->addChild('h'.$level);
+        ])->addChild('h'.$xlevel);
         $this->setText($title);
     }
+    /**
+     * Sets the text that will appear in the body of the heading.
+     * 
+     * @param HTMLNode|string $textOrNode This can be an object of type HTMLNode
+     * or a simple string.
+     */
     public function setText($textOrNode) {
         $this->headingNode->removeAllChildren();
 
@@ -24,6 +41,11 @@ class Heading extends HTMLNode {
             $this->headingNode->text($textOrNode);
         }
     }
+    /**
+     * Returns the note that contains heading text.
+     * 
+     * @return HTMLNode
+     */
     public function getHedingNode() {
         return $this->headingNode;
     }
